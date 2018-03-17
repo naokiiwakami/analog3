fc375ef33120a0cad4e9a9f76e9d76f7e0ffd4a9
Naoki Iwakami <naoki.iwakami@gmail.com>
2018-03-17 00:06:25 -0700

    #16 fixed valgrind error in unit test

--- src/server/test/module_test.cc
+++ src/server/test/module_test.cc
@@ -6,6 +6,7 @@ namespace analog3 {
 TEST(SynthNodeTest, Basic) {
   models::SynthNode* node = new models::Module();
   EXPECT_EQ(node->GetNodeType(), models::NodeType::MODULE);
+  delete node;
 }
 
 }  // namespace analog3
